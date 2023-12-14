d08ace65a903ce335f94e3aea319928c72d1aff0
Tushar Kadam <tushar_kadam@idealake.com>
2023-12-14 12:25:36 +0530

    Function Challenges

--- My First Query.sql
+++ My First Query.sql
@@ -719,7 +719,7 @@ WHERE
 
 --8
 
--- Return unique customers who have ordered the 'PBX Smart Watch 4’. Include:
+-- Return unique customers who have ordered the 'PBX Smart Watch 4ï¿½. Include:
 -- customer_id
 -- first_name
 -- last_name
@@ -743,3 +743,126 @@ WHERE
     WHERE 
       oes.products.product_name = 'PBX Smart Watch 4'
   );
+
+
+  -- Function Challenges
+
+
+--1
+
+-- Concatenate the first name and last name of each employee. Include a single space between the first and 
+-- last name. Name the new expression employee_name. Include:
+-- employee_id
+-- first_name
+-- last_name
+-- employee_name
+
+SELECT 
+  employee_id, 
+  first_name, 
+  last_name, 
+  CONCAT(first_name, ' ' + last_name) AS employee_name 
+FROM 
+  hcm.employees;
+
+
+--2
+
+-- Concatenate the first name, middle name, and last name of each employee. Include a single space between each 
+-- of the names. Name the new expression employee_name. Include:
+-- employee_id
+-- first_name
+-- last_name
+-- employee_name
+
+SELECT 
+  employee_id, 
+  first_name,
+  middle_name,
+  last_name, 
+  CONCAT(first_name, ' ' + middle_name, ' ' , last_name  ) AS employee_name 
+FROM 
+  hcm.employees;
+
+--3
+
+-- Extract the genus name from the scientific_name as given in the bird.antarctic_species table.
+
+SELECT 
+  LEFT(
+    scientific_name, 
+    CHARINDEX(' ', scientific_name)-1
+  ) AS genus_name 
+FROM 
+  bird.antarctic_species;
+
+--4
+
+-- Extract the species name from the scientific_name as given in the bird.antarctic_species table.
+
+SELECT 
+  SUBSTRING(
+    scientific_name, 
+    CHARINDEX(' ', scientific_name)+1,
+	LEN(scientific_name)
+  ) AS genus_name 
+FROM 
+  bird.antarctic_species;
+
+--5
+
+-- Return the age in years for all employees. Name this expression as employee_age. Include:
+-- employee_id
+-- first_name
+-- last_name
+-- birth_date
+-- employee_age
+
+
+SELECT 
+  employee_id, 
+  first_name, 
+  last_name, 
+  birth_date, 
+  DATEDIFF(
+    year, birth_date, CURRENT_TIMESTAMP
+  ) AS employee_age 
+FROM 
+  hcm.employees;
+
+--6
+
+-- Assuming an estimated shipping date of 7 days after the order date, add a column expression called 
+-- estimated_shipping_date for all unshipped orders. Include:
+-- order_id
+-- order_date
+-- estimated_shipping_date
+
+SELECT 
+  order_id, 
+  order_date, 
+  DATEADD(day, 7, order_date) AS estimated_shipping_date 
+FROM 
+  oes.orders 
+WHERE 
+  shipped_date IS NULL;
+
+--7
+
+-- Calculate the average number of days it takes each shipping company to ship an order. Call this expression 
+-- avg_shipping_days. Include:
+-- company_name
+-- avg_shipping_days
+
+SELECT 
+  s.company_name, 
+  AVG(
+    DATEDIFF(
+      day, o.order_date, o.shipped_date
+    )
+  ) AS avg_shipping_days 
+FROM 
+  oes.orders o 
+  JOIN oes.shippers s ON o.shipper_id = s.shipper_id 
+GROUP BY 
+  company_name;
