cb82d9970adde40c29001fb897ebada30c55d7ab
Tushar Kadam <tushar_kadam@idealake.com>
2023-12-19 12:10:17 +0530

    ALTER TABLE Challenges

--- My First Query.sql
+++ My First Query.sql
@@ -1384,3 +1384,48 @@ SELECT
   *
 FROM
   [oes].[bank_transactions]
+
+-- ALTER TABLE Challenges
+
+--1
+
+--Add a new column called ‘termination_date’ onto the 
+--hcm.employees table. Give this new column a data type of 
+--DATE.
+
+ALTER TABLE
+  hcm.employees
+ADD
+  termination_date DATE;
+	
+
+--2
+
+
+--Write two SQL statements to change the data type of the 
+--first_name and last_name columns to NVARCHAR(60) in the 
+--oes.customers table.
+
+ALTER TABLE
+  oes.customers
+ALTER COLUMN
+  first_name NVARCHAR(60);
+
+ALTER TABLE
+  oes.customers
+ALTER COLUMN
+  last_name NVARCHAR(60);
+
+--3
+
+--Use sp_rename to rename the column name 'phone' to 
+--'main_phone' in the oes.customers table.
+
+GO
+
+
+  sp_rename 'oes.customers.phone',
+  'main_phone',
+  'COLUMN';
+			 
+
